1961a050c665af02016f2802d9c0b130145f6a47
dennissammy77 <dennissammy77@gmail.com>
2025-04-20 16:59:31 +0300

    react tests

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 function App() {
-  return <div>Your code here</div>;
+  return (
+    <div>
+      <h1>Hi, I'm (your name)</h1>
+      <img src="" alt="profile image"/>
+      <h2>About Me</h2>
+      <p>A paragraph for your biography</p>
+      <a href="github">Github</a>
+      <a href="linkedin">LinkedIn</a>
+    </div>
+  );
 }
-
 export default App;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,3 +4,54 @@ import '@testing-library/jest-dom';
 import App from "../App";
 
 // Your tests here
+test("displays a top-level heading with the text `Hi, I'm _______`", () => {
+    // Arrange
+    render(<App />);
+    // Act
+    const topLevelHeading = screen.getByRole("heading", {
+        name: /hi, i'm/i,
+        exact: false,
+        level: 1,
+    });
+    // Assert
+    expect(topLevelHeading).toBeInTheDocument();
+});
+test("displays a second-level heading with the text `About Me`", () => {
+    // Arrange
+    render(<App />);
+    // Act
+    const secondLevelHeading = screen.getByRole("heading", {
+        name: /About Me/i,
+        exact: false,
+        level: 2,
+    });
+    // Assert
+    expect(secondLevelHeading).toBeInTheDocument();
+});
+test("displays a paragraph `A paragraph for your biography`", () => {
+    // Arrange
+    render(<App />);
+    // Act
+    const paragraph = screen.getByText(/A paragraph for your biography/i);
+    // Assert
+    expect(paragraph).toBeInTheDocument();
+});
+test("displays an image", () => {
+    // Arrange
+    render(<App />);
+    // Act
+    const image = screen.getByAltText(/profile image/i);
+    // Assert
+    expect(image).toBeInTheDocument();
+});
+test("displays a href", () => {
+    // Arrange
+    render(<App />);
+    // Act
+    const linkTags = screen.getAllByRole('link');
+    // Assert
+    expect(linkTags[0]).toBeInTheDocument();
+    expect(linkTags[1]).toBeInTheDocument();
+    expect(linkTags[0]).toHaveAttribute('href','github');
+    expect(linkTags[1]).toHaveAttribute('href','linkedin');
+});
